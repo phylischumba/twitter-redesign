d331229da3fcdfbce7247a57d5ddcb27968723f0
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-07-01 06:37:37 +0300

    add more helper methods for the likes model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5.hexdigest(user.username.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: 'gravatar')
+  end
+
+  
+    def menu_link_to(link_text, link_path)
+      class_name = current_page?(link_path) ? 'menu-item active' : 'menu-item'
+  
+      content_tag(:div, class: class_name) do
+        link_to link_text, link_path
+      end
+    end
+  
+    def like_or_dislike_btn(review)
+      like = Like.find_by(review: review, user: current_user)
+      if like
+        link_to('Dislike!', review_like_path(id: like.id, review_id: review.id), method: :delete)
+      else
+        link_to('Like!', review_likes_path(review_id: review.id), method: :review)
+      end
+    end
+  
 end
