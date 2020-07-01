5abb9c20e358a65a70cbf755a9c0dbf01ebda62b
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-07-01 06:41:16 +0300

    Add more columns abd tables to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_224450) do
+ActiveRecord::Schema.define(version: 2020_06_29_182135) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "review_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.text "Text"
