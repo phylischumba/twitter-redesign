2c84423c40223374296d9eb4c95bc1b1a779fb43
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-07-01 06:38:37 +0300

    Update the main routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, only: [:index, :create, :destroy, :show]
   resources :users
   root to: 'sessions#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :reviews do
+    resources :likes
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
